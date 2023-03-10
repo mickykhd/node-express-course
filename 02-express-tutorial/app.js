05b511436331e31b0139b539ba3d102de5410da8
mickykhd <mickykhd07@gmail.com>
2023-03-10 15:45:20 +0530

    ok

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end("<h1>Hello Express</h1>");
+});
+
+server.listen(5000);

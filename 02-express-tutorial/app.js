7223f00719a4d35b8d1786eeb2dadbd891c623f1
mickykhd <mickykhd07@gmail.com>
2023-03-10 17:42:31 +0530

    updated

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,27 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+const homePage = readFileSync("./navbar-app/index.html");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("<h1>Hello Express</h1>");
+  const url = req.url;
+  //home page
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>About</h1>");
+    res.end();
+  }
+  //404
+  else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>Page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
